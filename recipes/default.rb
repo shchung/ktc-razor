2fab3a576ec7727a895246f09096499f5d44d348
shchug <jung33@gmail.com>
2013-04-04 16:05:35 +0900

    Image and tag info moves to recipe(default)

--- recipes/default.rb
+++ recipes/default.rb
@@ -8,3 +8,32 @@
 #
 
 include_recipe 'razor'
+
+razor_image "razor-mk" do
+  type      "mk"
+  url       "https://downloads.puppetlabs.com/razor/iso/prod/rz_mk_prod-image.0.12.0.iso"
+  action    :add
+end
+razor_image "precise64" do
+  type      "os"
+  url       "http://ftp.daum.net/ubuntu-releases/12.04/ubuntu-12.04.2-server-amd64.iso"
+  version   "12.04"
+end
+# test code for tag add
+razor_tag "test2345" do
+	name		"testname2"
+	tag		"testtag2"
+	tag_matcher	 [{	"key"=>	"memsize",
+				"compare"=>	"equal",
+				"value"	=>	"1015mb",
+				"invert" =>	"false"
+			},
+		 	{	"key"=>		"cpu",
+				"compare"	=>	"equal",
+				"value"	=>	"15mb",
+				"invert"	=>	"false"
+			}]
+				
+				
+	action 	:add
+end

fa4f82c26afcc588046336ba42b0bb7294913726
Jesse Nelson <spheromak@gmail.com>
2013-10-21 19:22:57 -0700

    remove dead code

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,16 +1,2 @@
-#override['razor']['images'] = {
-#	'razor-mk' =>{
-#		'type' =>'mk',
-#		'url' => 'https://downloads.puppetlabs.com/razor/iso/prod/rz_mk_prod-image.0.12.0.iso',
-#		'action' => 'add'
-#	},
-#	'precise64' =>{
-#		'url'	=> 'http://ftp.daum.net/ubuntu-releases/12.04/ubuntu-12.04.2-server-amd64.iso',
-#		'version' => '12.04'
-#	}
-#}
 default['ktc-razor']['tag'] = Hash.new
 default['ktc-razor']['model'] = Hash.new
-
-	
-
